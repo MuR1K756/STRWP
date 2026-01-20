522e151e24ca18c6dcec229d0593df8a91336489
MuR1K756 <murenkov2006@mail.ru>
2026-01-20 22:27:15 +0300

    dsadada

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,12 +1,12 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'https://api-cs2-market.example.com', // Фейковый или реальный адрес
+  baseURL: 'http://localhost:3000/', 
   timeout: 5000,
   headers: { 'Content-Type': 'application/json' }
 });
 
-// Перехватчик: можно автоматически добавлять токен к каждому запросу
+
 api.interceptors.request.use((config) => {
   const user = localStorage.getItem('cs2_user');
   if (user) {
